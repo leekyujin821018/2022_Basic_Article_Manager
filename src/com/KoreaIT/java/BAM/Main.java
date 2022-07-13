b68ff307a497a90caff6ffa484d523cb2ce275f6
leekyujin821018 <leekyujin821018@gmail.com>
2022-07-13 17:28:21 +0900

    명령어 입력

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -7,10 +7,15 @@ public class Main {
 		System.out.println("==프로그램 시작==");
 		
 		Scanner sc = new Scanner(System.in);
-		
-		String command = sc.nextLine();
-		
-		System.out.printf("입력된 명령어 : %s\n", command);
+
+		while(true) {
+			System.out.printf("명령어 ) ");
+			String cmd = sc.nextLine();
+			
+			if(cmd.equals("exit")) {
+				break;
+			}
+		}
 		
 		System.out.println("==프로그램 끝==");
 		sc.close();
