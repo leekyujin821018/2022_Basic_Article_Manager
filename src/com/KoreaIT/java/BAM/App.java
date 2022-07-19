599038d0014b4d875f1f4905d716133452ab723b
leekyujin821018 <leekyujin821018@gmail.com>
2022-07-19 16:41:24 +0900

    ArrayList 순회하는 로직 중복 제거

--- src/com/KoreaIT/java/BAM/App.java
+++ src/com/KoreaIT/java/BAM/App.java
@@ -9,18 +9,18 @@ import com.KoreaIT.java.BAM.util.Util;
 
 public class App {
 	private List<Article> articles;
-	
+
 	public App() {
 		articles = new ArrayList<>();
 	}
-	
+
 	public void run() {
 		System.out.println("==프로그램 시작==");
-		
+
 		makeTestData();
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		while (true) {
 			System.out.printf("명령어 ) ");
 			String cmd = sc.nextLine().trim();
@@ -43,8 +43,8 @@ public class App {
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 
-					System.out.printf("%3d   |    %4s      |   %5s   | %5d\n", article.id, article.title, article.regDate,
-							article.hit);
+					System.out.printf("%3d   |    %4s      |   %5s   | %5d\n", article.id, article.title,
+							article.regDate, article.hit);
 				}
 
 			} else if (cmd.equals("article write")) {
@@ -62,50 +62,36 @@ public class App {
 				articles.add(article);
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
-			} else if (cmd.startsWith("article detail ")) {
+			}
+
+			else if (cmd.startsWith("article detail ")) {
 				String[] cmdBits = cmd.split(" ");
 
 				int id = Integer.parseInt(cmdBits[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
-				
+
 				foundArticle.increaseHit();
-				
+
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 				System.out.printf("조회 : %d\n", foundArticle.hit);
 
-			} else if (cmd.startsWith("article modify ")) {
+			}
+
+			else if (cmd.startsWith("article modify ")) {
 				String[] cmdBits = cmd.split(" ");
 
 				int id = Integer.parseInt(cmdBits[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -127,17 +113,7 @@ public class App {
 
 				int id = Integer.parseInt(cmdBits[2]);
 
-				int foundIndex = -1;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundIndex = i;
-						break;
-					}
-
-				}
+				int foundIndex = getArticleIndexById(id);
 
 				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -156,13 +132,52 @@ public class App {
 		sc.close();
 	}
 
+	private int getArticleIndexById(int id) {
+		int i = 0;
+		for (Article article : articles) {
+
+			if (article.id == id) {
+				return i;
+			}
+			i++;
+		}
+
+		return -1;
+	}
+
+	private Article getArticleById(int id) {
+//		== v3 ==
+		int index = getArticleIndexById(id);
+
+		if (index != -1) {
+			return articles.get(index);
+		}
+		
+//		== v1 ==
+//		for (int i = 0; i < articles.size(); i++) {
+//			Article article = article.get(i);
+//			
+//			if (article.id == id) {
+//				return article;
+//			}
+//		}
+//		== v2 ==
+//		for (Article article : articles) {
+		
+//			if (article.id == id) {
+//				return article;
+//			}
+//		}
+
+		return null;
+	}
+
 	private void makeTestData() {
 		System.out.println("테스트를 위한 게시물 데이터를 생성합니다.");
-		
+
 		articles.add(new Article(1, Util.getNowDateStr(), "제목1", "내용1", 11));
 		articles.add(new Article(2, Util.getNowDateStr(), "제목2", "내용2", 22));
 		articles.add(new Article(3, Util.getNowDateStr(), "제목3", "내용3", 33));
 	}
-	
 
 }
